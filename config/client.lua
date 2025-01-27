857b8c71eeb92b1642dfede0d86612bf05fcdb0e
Dooms192 <31485904+Dooms192@users.noreply.github.com>
2025-01-26 21:22:34 -0500

    Updated config

--- config/client.lua
+++ config/client.lua
@@ -2,8 +2,8 @@ return {
     debugPoly = false,
     useDrawText = false,
     openRegisterTime = 24000,
-    policeAlertChance = 0.70,
-    policeNightAlertChance = 0.40,
+    policeAlertChance = 0.80,
+    policeNightAlertChance = 0.70,
 
     fingerprintChance = 50, -- Chance of dropping a fingerprint if not wearing gloves
 }
